d2df26889140b92961570fd42645b667c9105e5a
Nic-Mon <Nic@calvisitor-10-105-160-147.calvisitor.1918.berkeley.edu>
2016-08-16 13:07:39 -0700

    Nic's D10 files

--- numbered_lines.py
+++ numbered_lines.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+def numbered_lines(filename):
+	with open(filename, 'r') as fin:
+		lines = fin.readlines()
+	with open('new_' + filename, 'w') as fout:
+		n = 1
+		for line in lines:
+			if line != '\n':
+				fout.write(str(n) + " " + line)
+			n += 1
 
 def main():
-    numbered_lines()
+    numbered_lines('small.txt')
 
 if __name__ == "__main__":
     main()
